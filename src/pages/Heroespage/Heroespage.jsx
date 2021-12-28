8c8c5c586bc5f586e1ee58ed16db985f673534d4
unknown <ermolov1337@gmail.com>
2021-12-28 17:02:09 +0300

    add error API

--- src/pages/Heroespage/Heroespage.jsx
+++ src/pages/Heroespage/Heroespage.jsx
@@ -2,25 +2,29 @@ import React, { useState, useEffect } from "react";
 import { getApiResource } from "../../utils/network";
 import { HEROES_DATA } from "../../constants/api";
 import Heroeslist from "../../components/Heroeslist/Heroeslist";
+import { withErrorApi } from "../../hoc-helpers/withErrorApi";
 
-export default function Heroespage() {
+const Heroespage = ({ setErrorApi }) => {
   const [heroes, setHeroes] = useState(null);
 
   const getResource = async (url) => {
     const res = await getApiResource(url);
-    setHeroes(res);
     console.log(res);
+    
+
+    if (res) {
+      setHeroes(res);
+      setErrorApi(false);
+    } else {
+      setErrorApi(true);
+    }
   };
 
   useEffect(() => {
     getResource(HEROES_DATA);
   }, []);
 
-    return (
-        
-    <div>
-            {heroes && <Heroeslist data={heroes} />}
-    </div>
-        
-    )
-}
+  return <div>{heroes && <Heroeslist data={heroes} />}</div>;
+};
+
+export default withErrorApi(Heroespage);
